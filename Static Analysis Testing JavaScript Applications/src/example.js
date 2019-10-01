95de37aa79434be1b7035ece056c74ad874c0943
Spencer <spencerranney@gmail.com>
2019-10-01 00:33:12 -0500

    testing stuff

--- Static Analysis Testing JavaScript Applications/src/example.js
+++ Static Analysis Testing JavaScript Applications/src/example.js
@@ -2,6 +2,15 @@ const nameofperson = 'Spencer';
 typeof nameofperson === "string";
 
 const greeting = "hello";
-console.log(`${greeting} world`);
+console
+
+
+
+
+
+
+
+
+.log(`${greeting} world`);
 
 [(1, 2, 3)].forEach(x => console.log(x));
